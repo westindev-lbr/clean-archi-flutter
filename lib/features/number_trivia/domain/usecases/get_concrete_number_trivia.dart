5815b6773e7dfce15a6c9ec1b16f678e6064130a
Axel Labarre <axel.labarre@gmail.com>
2023-05-08 00:23:01 +0200

    Domain layer + tests OK

--- lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -1,15 +1,27 @@
-import 'package:clean_archi_flutter_app/features/number_trivia/domain/entities/number_trivia.dart';
-import 'package:clean_archi_flutter_app/features/number_trivia/domain/repositories/number_trivia_repository_interface.dart';
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 
 import '../../../../core/error/failures.dart';
+import '../../../../core/usecases/usecase.dart';
+import '../entities/number_trivia.dart';
+import '../repositories/number_trivia_repository_interface.dart';
 
-class GetConcreteNumberTrivia {
+class GetConcreteNumberTrivia implements UseCase<NumberTrivia, Params> {
   final INumberTriviaRepository repository;
 
   GetConcreteNumberTrivia(this.repository);
 
-  Future<Either<Failure, NumberTrivia>?> execute({required int number}) async {
-    return await repository.getConcreteNumberTrivia(number);
+  @override
+  Future<Either<Failure, NumberTrivia>?> call(Params params) async {
+    return await repository.getConcreteNumberTrivia(params.number);
   }
+}
+
+class Params extends Equatable {
+  final int number;
+
+  const Params({required this.number});
+
+  @override
+  List<Object?> get props => [number];
 }
--- test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
@@ -1,6 +1,6 @@
 import 'package:clean_archi_flutter_app/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:clean_archi_flutter_app/features/number_trivia/domain/repositories/number_trivia_repository_interface.dart';
-import 'package:clean_archi_flutter_app/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:clean_archi_flutter_app/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 import 'package:dartz/dartz.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
@@ -11,29 +11,27 @@ class MockNumberTriviaRepository extends Mock
 
 @GenerateMocks([INumberTriviaRepository])
 void main() {
-  late GetConcreteNumberTrivia usecase;
+  late GetRandomNumberTrivia usecase;
   late MockNumberTriviaRepository mockNumberTriviaRepository;
-  late int tNumber;
   late NumberTrivia tNumberTrivia;
 
   setUp(() {
     mockNumberTriviaRepository = MockNumberTriviaRepository();
-    usecase = GetConcreteNumberTrivia(mockNumberTriviaRepository);
-    tNumber = 1;
+    usecase = GetRandomNumberTrivia(mockNumberTriviaRepository);
     tNumberTrivia =
         const NumberTrivia(text: 'Naissance de Calum', number: 2012);
   });
 
-  test('Should get trivia for the number from the repository', () async {
+  test('Should get trivia from the repository', () async {
     // arrange (on veut que quand on passe tNumber en param qu'on retourne notre tNumberTrivia)
-    when(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber))
+    when(mockNumberTriviaRepository.getRandomNumberTrivia())
         .thenAnswer((_) async => Right(tNumberTrivia));
     // act (On applique simplement notre usecase avec un paramètre au choix)
-    final result = await usecase(Params(number: tNumber));
+    final result = await usecase(NoParams());
     // assert (on vérifie si c'est le bon résultat attendu)
     expect(result, Right(tNumberTrivia));
     // ici on vérifie si la méthode a été appelée avec le bon paramètre
-    verify(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber));
+    verify(mockNumberTriviaRepository.getRandomNumberTrivia());
     verifyNoMoreInteractions(mockNumberTriviaRepository);
   });
 }
